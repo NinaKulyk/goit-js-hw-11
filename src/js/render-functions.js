a7698d68535f7fc6c376a8ac95e48edc5a7e3602
NinaKulyk <nina1998lukashova@gmail.com>
2024-06-02 14:55:10 +0100

    update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,43 +1,27 @@
-
 import iziToast from 'izitoast';
 
-export function renderGallery(images) {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = images.map(image => `
-    <a href="${image.largeImageURL}" class="gallery__item">
-      <img src="${image.webformatURL}" alt="${image.tags}" class="gallery__image" />
-      <div class="info">
-        <p class="info-item">Likes: <span>${image.likes}</span></p>
-        <p class="info-item">Views: <span>${image.views}</span></p>
-        <p class="info-item">Comments: <span>${image.comments}</span></p>
-        <p class="info-item">Downloads: <span>${image.downloads}</span></p>
-      </div>
-    </a>
-  `).join('');
-}
-
 export function showLoader() {
-  document.querySelector('.loader').style.display = 'block';
 }
 
 export function hideLoader() {
-  document.querySelector('.loader').style.display = 'none';
 }
 
 export function clearGallery() {
-  document.querySelector('.gallery').innerHTML = '';
+}
+
+export function renderGallery(images) {
 }
 
 export function showErrorToast(message) {
   iziToast.error({
     title: 'Error',
-    message: message
+    message: message,
   });
 }
 
 export function showInfoToast(message) {
   iziToast.info({
     title: 'Info',
-    message: message
+    message: message,
   });
 }
